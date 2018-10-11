187ccf79f9f44a2eb276ddd6680a695384759b23
Daniel-easj <31881808+Daniel-easj@users.noreply.github.com>
2018-10-11 19:19:58 +0200

    Minor change

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,20 +3,32 @@ import axios from 'axios';
 
 let buttonGet:HTMLButtonElement = <HTMLButtonElement> document.getElementById("buttonGet");
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("responseElement");
+let element1: HTMLDivElement = <HTMLDivElement> document.getElementById("responseElement");
+let detailedViewElement: HTMLDivElement = <HTMLDivElement> document.getElementById("responseElement");
 
 let value : HTMLInputElement = <HTMLInputElement>document.getElementById("valueInput");
+
+
 
 
 buttonGet.addEventListener("click", MouseEvent => {
     axios.get('https://jsonplaceholder.typicode.com/users/')
   .then(function (response) {
-    element.innerHTML = "<p>" + JSON.stringify(response.data[value.valueAsNumber].name) + "</p>";
-    element.innerHTML += "<p>" + JSON.stringify(response.data[value.valueAsNumber].email) + "</p>";
-    console.log(response.data[0].title);
+        for(var _i = 0; _i < 10; _i++){
+            element1.innerHTML += "<p>" + JSON.stringify(response.data[_i].name) + "</p>";
+            element1.innerHTML += '<button id="' + JSON.stringify(response.data[_i].id) 
+            + ' onclick="DetailedView(' + JSON.stringify(response.data[_i].id) + ')"'
+            +  '">'
+            + "Detailed view" + "</button>";
+
+        }
   })
   .catch(function (error) {
     console.log(error);
   });
 })
+
+function detailedView(id:number){
+        
+}
 
