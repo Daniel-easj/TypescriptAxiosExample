3107cd6aeb04df50a337c5f9b2cd2dd4af1655ad
Daniel-easj <31881808+Daniel-easj@users.noreply.github.com>
2018-10-11 12:21:42 +0200

    minor change

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,34 +1,18 @@
 import axios from 'axios';
 
-class todo{
-    _userid: number;
-    _id: number;
-    _title: string;
-    _completed: boolean;
-
-    constructor(userid: number, id: number, title: string, completed:boolean){
-        this._userid = userid;
-        this._id = id;
-        this._title = title;
-        this._completed = completed;
-    }
-
-    /**
-     * ToString
-     */
-    public ToString(): string {
-        return this._userid + " " + this._title;
-    }
-}
 
 let buttonGet:HTMLButtonElement = <HTMLButtonElement> document.getElementById("buttonGet");
 
 let element: HTMLDivElement = <HTMLDivElement> document.getElementById("responseElement");
 
+let value : HTMLInputElement = <HTMLInputElement>document.getElementById("valueInput");
+
+
 buttonGet.addEventListener("click", MouseEvent => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/')
+    axios.get('https://jsonplaceholder.typicode.com/users/')
   .then(function (response) {
-    element.innerHTML = "JSON: " + JSON.stringify(response.data[0].title);
+    element.innerHTML = "<p>" + JSON.stringify(response.data[value.valueAsNumber].name) + "</p>";
+    element.innerHTML += "<p>" + JSON.stringify(response.data[value.valueAsNumber].email) + "</p>";
     console.log(response.data[0].title);
   })
   .catch(function (error) {
