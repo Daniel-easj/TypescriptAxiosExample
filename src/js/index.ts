e2d48649b22172acf4a33b98ed9ff4be36450580
Daniel-easj <31881808+Daniel-easj@users.noreply.github.com>
2018-10-11 10:26:07 +0200

    Get a title from element

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,38 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios from 'axios';
+
+class todo{
+    _userid: number;
+    _id: number;
+    _title: string;
+    _completed: boolean;
+
+    constructor(userid: number, id: number, title: string, completed:boolean){
+        this._userid = userid;
+        this._id = id;
+        this._title = title;
+        this._completed = completed;
+    }
+
+    /**
+     * ToString
+     */
+    public ToString(): string {
+        return this._userid + " " + this._title;
+    }
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+let buttonGet:HTMLButtonElement = <HTMLButtonElement> document.getElementById("buttonGet");
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+let element: HTMLDivElement = <HTMLDivElement> document.getElementById("responseElement");
+
+buttonGet.addEventListener("click", MouseEvent => {
+    axios.get('https://jsonplaceholder.typicode.com/todos/')
+  .then(function (response) {
+    element.innerHTML = "JSON: " + JSON.stringify(response.data[0].title);
+    console.log(response.data[0].title);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+})
+
