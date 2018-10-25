4bdb3473e6340381370e1c965a87a44b059f9af6
Daniel-easj <31881808+Daniel-easj@users.noreply.github.com>
2018-10-25 09:04:00 +0200

    Smarter p tags

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,7 +4,8 @@ import axios from 'axios';
 let buttonGet:HTMLButtonElement = <HTMLButtonElement> document.getElementById("buttonGet");
 
 let element1: HTMLDivElement = <HTMLDivElement> document.getElementById("responseElement");
-let detailedViewElement: HTMLDivElement = <HTMLDivElement> document.getElementById("responseElement");
+
+let detailedViewElement: HTMLDivElement = <HTMLDivElement> document.getElementById("detailedViewDiv");
 
 let value : HTMLInputElement = <HTMLInputElement>document.getElementById("valueInput");
 
@@ -17,7 +18,7 @@ buttonGet.addEventListener("click", MouseEvent => {
         for(var _i = 0; _i < 10; _i++){
             element1.innerHTML += "<p>" + JSON.stringify(response.data[_i].name) + "</p>";
             element1.innerHTML += '<button id="' + JSON.stringify(response.data[_i].id) 
-            + ' onclick="DetailedView(' + JSON.stringify(response.data[_i].id) + ')"'
+            + ' onclick= "DetailedView('+ JSON.stringify(response.data[_i].id) +')'
             +  '">'
             + "Detailed view" + "</button>";
 
@@ -29,6 +30,11 @@ buttonGet.addEventListener("click", MouseEvent => {
 })
 
 function detailedView(id:number){
-        
+  axios.get('https://jsonplaceholder.typicode.com/users/')
+  .then(function (response) {
+    detailedViewElement.innerHTML += "<p>" + JSON.stringify(response.data[id].name) + "</p>";
+    detailedViewElement.innerHTML += "<p>" + JSON.stringify(response.data[id].email) + "</p>";
+  })
+      
 }
 
